5044813ef3960b3008ead601a1651f0e86f3b05d
jaclay1994 <jaclay94@gmail.com>
2021-03-20 23:31:11 -0500

    Updated Files.

--- models/index.js
+++ models/index.js
@@ -3,15 +3,40 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const seedProductTags = require('../seeds/product-tag-seeds');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product'
+});
 // Categories have many Products
-
+Category.belongsToMany(Product, {
+  through: {
+    model: Tag,
+    unique: false
+  },
+  as: 'category'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tag1'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tag2'
+})
 module.exports = {
   Product,
   Category,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,49 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try {
+    const productData = await Product.findAll(req.params.id, {
+      include: [{ model: Category, through: Tag, as: 'category' }],
+      include: [{ model: Tag, through: ProductTag, as: 'product_tag2' }]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category, through: Tag, as: 'category' }],
+      include: [{ model: Product, through: ProductTag, as: 'product' }]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No category available.' });
+      return;
+    }
+    res.status(200).json(productData);
+    // be sure to include its associated Category and Tag data
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const productData = await Product.create(req.body);
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(400).json(err);
+  }
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -48,7 +78,8 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+
   // update product data
   Product.update(req.body, {
     where: {
@@ -89,8 +120,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(400).json({ message: 'No Data Available!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
